e77e98095f0ac963526c27af108ffb9e41d3d36a
twof <fabiobean2@gmail.com>
2023-12-18 14:07:47 -0800

    Initial Commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by fnord on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
